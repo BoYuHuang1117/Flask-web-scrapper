736a098169511d07e55427a0d464568696b0b0c2
BoYu Huang <boyuhuang.michael@gmail.com>
2023-02-20 15:17:33 -0600

    Draft first web scrape

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask import jsonify
 
+from services.scrapeWeb1 import WebSpiderAMP
+from services.scrapeWeb2 import WebSpiderLSVP
 from settings import Config
 
-app = Flask("scraper")
+app = Flask(__name__)
 app.config.from_object(Config)
+
+""" API definition """
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify(msg="Welcome to Michael's scraper!!")
+
+
+@app.route("/api/scrape-all", methods=["GET"])
+def scrape():
+    # "https://www.amplifypartners.com/portfolio"
+    # "https://lsvp.com/portfolio/"
+    response = {}
+
+
+    return jsonify(response)
---
+++ services/scrapeWeb1.py
@@ -0,0 +1,20 @@
+import scrapy
+
+# Current commmand to run "scrapy runspider .\scrapeWeb1.py"
+class WebSpiderAMP(scrapy.Spider):
+    name = "webspideramp"
+    start_urls = ["https://www.amplifypartners.com/portfolio"]
+
+    # .status-div
+    # .website__link-wr
+    
+    def parse(self, response):
+        ans = []
+        
+        for title, website in zip(response.css(".status-div"), response.css(".website__link-wr")):
+            status = title.css("::text").get().strip()
+            website = website.css("::text").get().stripe()
+            if status == "Active":
+                ans.append(website)
+        
+        return ans
